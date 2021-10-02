ef3ca99ee49fa0071e2b297191b3199b227fe0fc
CharwekarAkshay <charwekar12@gmail.com>
2021-10-02 13:17:20 +0530

    Backend server added

---
+++ design-backend/src/test/java/com/coldcoder/designbackend/DesignBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coldcoder.designbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
