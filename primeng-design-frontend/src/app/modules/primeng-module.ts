6b8c1b708e74787ca3562674ae045e3b238f1bd8
AkshayCharwekar <Akshay.Charwekar@dh.com>
2021-10-15 22:00:32 +0530

    Added basic theming login

--- primeng-design-frontend/src/app/modules/primeng-module.ts
+++ primeng-design-frontend/src/app/modules/primeng-module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 @NgModule({
     imports: [
         MenuModule,
         ButtonModule,
+        CardModule
     ],
     exports: [
         MenuModule,
         ButtonModule,
+        CardModule
     ]
 })
 export class PrimengModule { }
