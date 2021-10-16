76a38bb9d766147d1e2fef04b0d293db270a4370
CharwekarAkshay <charwekar12@gmail.com>
2021-10-16 23:01:50 +0530

    Performacne changes

--- primeng-design-frontend/src/app/components/screens/home-screen/home-screen.component.ts
+++ primeng-design-frontend/src/app/components/screens/home-screen/home-screen.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { pluck } from 'rxjs/operators';
 import { Movie } from 'src/app/models/movie';
+import { ProgressService } from 'src/app/services/progress.service';
 
 @Component({
   selector: 'app-home-screen',
@@ -10,7 +11,7 @@ import { Movie } from 'src/app/models/movie';
 })
 export class HomeScreenComponent implements OnInit {
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService, private progressService: ProgressService) { }
 
   public movieList: Array<Movie> = [];
 
@@ -19,16 +20,16 @@ export class HomeScreenComponent implements OnInit {
   }
 
   fetchData(): void {
+    this.progressService.showProgressBar();
     this.dataService.getTrendingMovies().pipe(
       pluck("content")
     ).subscribe((list) => {
+      this.progressService.hideProgressBar();
       if (list) {
         console.log(list);
         this.movieList = list as Array<Movie>;
       }
     });
   }
-
-  
 
 }
